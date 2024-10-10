51e80eb6a1f1490cad2a773ed3e4b67031ff9397
Limtotage <abdullahltgyalim@gmail.com>
2024-10-10 10:42:24 +0300

    Create artioreksi.js

---
+++ artioreksi.js
@@ -0,0 +1,9 @@
+let sayi = prompt("Bir sayi girin:");
+
+if (sayi > 0) {
+  console.log("Pozitif Bir Sayi Girdiniz.");
+} else if (sayi < 0) {
+  console.log("Negatif Bir Sayi Girdiniztir");
+} else {
+  console.log("Sıfır Rakamını Girdiniz.");
+}
