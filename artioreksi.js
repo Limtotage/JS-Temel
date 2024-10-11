36fc2b5bbee9373bd98fa303f32ae0e4fa5dc5d9
Limtotage <abdullahltgyalim@gmail.com>
2024-10-11 11:46:21 +0300

    Delete artioreksi.js

--- artioreksi.js
+++
@@ -1,9 +0,0 @@
-let sayi = prompt("Bir sayi girin:");
-
-if (sayi > 0) {
-  console.log("Pozitif Bir Sayi Girdiniz.");
-} else if (sayi < 0) {
-  console.log("Negatif Bir Sayi Girdiniztir");
-} else {
-  console.log("Sifir Rakamini Girdiniz.");
-}
