2481cacc67481227d383804087554fb3215e0a20
Limtotage <abdullahltgyalim@gmail.com>
2024-10-11 14:06:43 +0300

    Update JS-Basit-Sayı-Tahmin-Oyunu.js

--- JS-Basit-Sayı-Tahmin-Oyunu.js
+++ JS-Basit-Sayı-Tahmin-Oyunu.js
@@ -5,7 +5,7 @@ let tahmin = 0;
 let rastgele_sayi = Math.floor(Math.random() * 100) + 1;
 console.log("1 ile 100 arasinda bir sayi sectim! Bil Bakalim Hangis SAyi :D");
 function tahmin_yap() {
-  let Girdi = prompt("Lutfen ${min} ile ${max} arasinda bir sayi tahmin ediniz: ");
+  let Girdi = prompt(`Lutfen ${min} ile ${max} arasinda bir sayi tahmin ediniz: `);
   tahmin = parseInt(Girdi);
   if (isNaN(tahmin) || tahmin < min || tahmin > max) {
     console.log("Sayi Gecersizdir! Lutfen Gecerli Bir Sayi Giriniz.");
@@ -13,7 +13,7 @@ function tahmin_yap() {
   }
   deneme_say++;
   if (tahmin === rastgele_sayi) {
-    console.log("Tebrikler!??! ${tahmin} Dogru Tahmindi. Toplam ${deneme_say} denemede buldunuz.");
+    console.log(`Tebrikler!??! ${tahmin} Dogru Tahmindi. Toplam ${deneme_say} denemede buldunuz.`);
     return;
   }
   if (tahmin < rastgele_sayi) {
